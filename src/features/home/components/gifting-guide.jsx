b489b60081119d313f0d135d5ea5c07f5aafb591
shiv-orpol <shibaramorpolinfotech@gmail.com>
2025-04-12 15:21:58 +0530

    #dev gifting guide swiper coverflow finished.... .... ..

--- src/features/home/components/gifting-guide.jsx
+++ src/features/home/components/gifting-guide.jsx
@@ -3,6 +3,7 @@
 import { EffectCoverflow, Mousewheel, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Heading from '@/components/heading';
+import { useEffect, useState } from 'react';
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -10,106 +11,64 @@ import 'swiper/css/pagination';
 import Image from 'next/image';
 
 export default function GiftingSection() {
+  const [coverflowConfig, setCoverflowConfig] = useState({
+    depth: 450,
+    scale: 0.9,
+    spaceBetween: -160
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 767) {
+        setCoverflowConfig({ depth: 800, scale: 1.25, spaceBetween: 30 });
+        console.log('desktop');
+      }
+      // } else {
+      //   setCoverflowConfig({ depth: 450, scale: 0.9, spaceBetween: -160 });
+      //   console.log('mobile');
+      // }
+    };
+
+    handleResize(); // on mount
+    window.addEventListener('resize', handleResize); // on resize
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  console.log(coverflowConfig);
   return (
     <section className='pt-9 md:pt-12 lg:pt-16 xl:pt-20'>
       <Heading
         title='Gifting Guide'
         subtitle='Statement pieces fit for royalty'
       />
-      <div className='mx-auto my-10 h-fit w-full max-w-[1640px] overflow-hidden border border-black'>
-        <Swiper
-          modules={[EffectCoverflow]}
-          effect={'coverflow'}
-          loop={true}
-          spaceBetween={30}
-          slidesPerView={2.3}
-          centeredSlides={true}
-          grabCursor={true}
-          coverflowEffect={{
-            rotate: 0,
-            slideShadows: false,
-            depth: 800,
-            scale: 1
-          }}
-          className='coverflow'
-        >
-          {Array.from({ length: 5 }).map((_, index) => (
-            <SwiperSlide key={index} className='border border-red-500'>
-              <div className='h-[400px] rounded-sm bg-green-200'>
-                <Image
-                  src={`/img/gifting/guide${index + 1}.png`}
-                  fill={true}
-                  className='rounded-2xl object-cover'
-                  alt='Guide image'
-                />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <div className='wrapper'>
-        <div className='mx-auto my-10 h-fit w-[70%] overflow-hidden border border-black lg:w-full'>
-          <Swiper
-            modules={[EffectCoverflow]}
-            effect={'coverflow'}
-            loop={true}
-            spaceBetween={30}
-            slidesPerView={2.3}
-            centeredSlides={true}
-            grabCursor={true}
-            coverflowEffect={{
-              rotate: 0,
-              slideShadows: true,
-              depth: 800,
-              scale: 1.25
-            }}
-            className='coverflow'
-          >
-            {Array.from({ length: 5 }).map((_, index) => (
-              <SwiperSlide key={index} className='border border-red-500'>
-                <div className='flex h-[350px] items-center justify-center rounded-sm bg-green-200 text-xl'>
-                  {/* <Image
-                  src={`/img/gifting/guide${index + 1}.png`}
-                  height={400}
-                  width={700}
-                  className='h-full w-full rounded-sm bg-cover'
-                  alt='Guide image'
-                /> */}
-                  desktop
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </div>
       <div className=''>
-        <div className='mx-auto my-10 h-fit w-full max-w-lg overflow-hidden border border-black px-2'>
+        <div className='wrapper mx-auto my-10 h-fit overflow-hidden max-[440px]:px-[6px]'>
           <Swiper
+            key={JSON.stringify(coverflowConfig)} // <- force re-init on config change
             modules={[EffectCoverflow]}
             effect={'coverflow'}
             loop={true}
-            spaceBetween={-160}
+            spaceBetween={coverflowConfig.spaceBetween}
             slidesPerView={2.3}
             centeredSlides={true}
             grabCursor={true}
             coverflowEffect={{
               rotate: 0,
               slideShadows: true,
-              depth: 500,
-              scale: 0.9
+              depth: coverflowConfig.depth,
+              scale: coverflowConfig.scale
             }}
             className='coverflow'
           >
             {Array.from({ length: 5 }).map((_, index) => (
-              <SwiperSlide key={index} className='border border-red-500'>
-                <div className='flex h-[170px] w-auto items-center justify-center rounded-sm bg-green-200 text-xl'>
+              <SwiperSlide key={index} className=''>
+                <div className='relative aspect-[240/170] w-auto rounded-sm'>
                   <Image
                     src={`/img/gifting/guide${index + 1}.png`}
                     fill={true}
-                    className='h-full w-full rounded-xl object-cover'
+                    className='rounded-xl object-cover'
                     alt='Guide image'
                   />
-                  <p className='absolute inset-x-0 bottom-0 border-t border-white p-1 text-center text-sm font-medium text-white sm:text-sm lg:text-xl'>
+                  <p className='absolute inset-x-0 bottom-0 mx-4 border-t border-white pt-1 pb-2 text-sm text-white sm:text-sm md:text-lg md:font-medium lg:pt-2 lg:pb-4 lg:text-2xl'>
                     Eternal Rings (400 Items)
                   </p>
                 </div>
@@ -118,7 +77,8 @@ export default function GiftingSection() {
           </Swiper>
         </div>
       </div>
-      <div className='wrapper'>
+      {/* mobile & desktop */}
+      {/* <div className='wrapper'>
         <div className='mx-auto my-10 h-fit w-full max-w-lg overflow-hidden border border-black'>
           <Swiper
             modules={[EffectCoverflow]}
@@ -139,150 +99,77 @@ export default function GiftingSection() {
             {Array.from({ length: 5 }).map((_, index) => (
               <SwiperSlide key={index} className='border border-red-500'>
                 <div className='flex h-[140px] items-center justify-center rounded-sm bg-green-200 text-xl'>
-                  {/* <Image
-                  src={`/img/gifting/guide${index + 1}.png`}
-                  height={400}
-                  width={700}
-                  className='h-full w-full rounded-sm bg-cover'
-                  alt='Guide image'
-                /> */}
                   mobile
                 </div>
               </SwiperSlide>
             ))}
           </Swiper>
         </div>
-      </div>
+      </div> */}
+      {/* <div className='wrapper'>
+        <div className='mx-auto my-10 h-fit overflow-hidden border border-black'>
+          <Swiper
+            modules={[EffectCoverflow]}
+            effect={'coverflow'}
+            loop={true}
+            spaceBetween={30}
+            slidesPerView={2.3}
+            centeredSlides={true}
+            grabCursor={true}
+            coverflowEffect={{
+              rotate: 0,
+              slideShadows: true,
+              depth: 800,
+              scale: 1.25
+            }}
+            className='coverflow'
+          >
+            {Array.from({ length: 5 }).map((_, index) => (
+              <SwiperSlide key={index} className='border border-red-500'>
+                <div className='flex h-[350px] items-center justify-center rounded-sm bg-green-200 text-xl'>
+                  desktop
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div> */}
+      {/* desktop */}
+      {/* <div className='wrapper mx-auto my-10 h-fit overflow-hidden border border-black max-[440px]:px-[6px]'>
+        <div>scale: {coverflowConfig.scale} </div>
+        <Swiper
+          modules={[EffectCoverflow]}
+          effect={'coverflow'}
+          loop={true}
+          spaceBetween={30}
+          slidesPerView={2.3}
+          centeredSlides={true}
+          grabCursor={true}
+          coverflowEffect={{
+            rotate: 0,
+            slideShadows: true,
+            depth: 800,
+            scale: 1.25
+          }}
+          className='coverflow'
+        >
+          {Array.from({ length: 5 }).map((_, index) => (
+            <SwiperSlide key={index} className='border border-red-500'>
+              <div className='relative aspect-[240/170] w-auto rounded-sm bg-green-200'>
+                <Image
+                  src={`/img/gifting/guide${index + 1}.png`}
+                  fill={true}
+                  className='rounded-xl object-cover'
+                  alt='Guide image'
+                />
+                <p className='absolute inset-x-0 bottom-0 mx-4 border-t border-white pt-1 pb-2 text-sm text-white sm:text-sm md:text-lg md:font-medium lg:pt-2 lg:pb-4 lg:text-2xl'>
+                  Eternal Rings (400 Items)
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div> */}
     </section>
   );
 }
-
-// ------------------------------------------ rough work ------------------------------------------
-{
-  /* <Swiper
-modules={[EffectCoverflow, Pagination]}
-effect={'coverflow'}
-loop={true}
-spaceBetween={10}
-slidesPerView={5}
-pagination={{
-  clickable: true
-}}
-centeredSlides={true}
-grabCursor={true}
-coverflowEffect={{
-  rotate: 0,
-  slideShadows: false
-}}
-className='coverflow border border-red-500'
->
-{Array.from({ length: 5 }).map((_, index) => {
-  return (
-    <SwiperSlide key={index} className='border border-red-500'>
-      <div className='h-[350px] w-[600px] rounded-sm'>
-        <Image
-          src={`/img/gifting/guide${index + 1}.png`}
-          height={400}
-          width={700}
-          className='h-full w-full rounded-sm bg-cover'
-          alt='Guide image'
-        />
-        <div className='mx-4 -translate-y-16 border-t-2 border-t-white p-4 text-2xl font-semibold text-white'>
-          Eternal Rings (400 items)
-        </div>
-      </div>
-    </SwiperSlide>
-  );
-})}
-</Swiper> */
-}
-
-// import Image from 'next/image';
-
-// export default function GiftingGuide() {
-//   return (
-//     <section className='wrapper pt-10'>
-//       <Heading
-//         title='Gifting Guide'
-//         subtitle='Statement pieces fit for royalty'
-//       />
-//       <div>
-//         <div className='border'>
-//           <Image
-//             src='/img/gifting/guide1.png'
-//             height={420}
-//             width={700}
-//             className='h-full w-full'
-//             alt='Guide image'
-//           />
-//           <div className='mx-4 -translate-y-16 border-t-2 border-t-white p-4 text-2xl font-semibold text-white'>
-//             Eternal Rings (400 items)
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-/* ------------------------------------------ rough work ------------------------------------------
-<Swiper
-modules={[EffectCoverflow, Mousewheel, Autoplay]}
-effect='coverflow'
-loop={true}
-spaceBetween={-50} // Adjust spacing for proper centering
-slidesPerView={5} // Dynamically sizes slides
-centeredSlides={true} // Centers the active slide
-grabCursor={true}
-coverflowEffect={{
-  rotate: 0,
-  stretch: 20,
-  depth: 100, // Makes side slides more visible
-  modifier: 1.2, // Controls scale intensity
-  slideShadows: false
-}}
-autoplay={{
-  delay: 3000, // 2.5 seconds between slides
-  disableOnInteraction: false, // keeps autoplay even after user interaction
-  pauseOnMouseEnter: true // optional: pause when hovered
-}}
-mousewheel={{ invert: true }}
-breakpoints={{
-  320: {
-    // when window width is >= 320px
-    slidesPerView: 2,
-    spaceBetween: -20
-  },
-  425: {
-    slidesPerView: 3,
-    spaceBetween: -20
-  },
-  768: {
-    slidesPerView: 3,
-    spaceBetween: -40
-  },
-  1024: {
-    slidesPerView: 5,
-    spaceBetween: -50
-  }
-}}
-className='coverflow relative w-full'
->
-<div>
-  {Array.from({ length: 5 }).map((_, index) => (
-    <SwiperSlide key={index} className='-mx-[10px] pb-6 lg:mx-[60px]'>
-      <div className='w xs:h-[160px] xs:w-[260px] xl2:h-[490px] relative aspect-[4/3] h-[140px] w-[208px] overflow-hidden rounded-sm sm:h-[180px] sm:w-[310] md:h-[300px] md:w-[400px] lg:h-[360px] lg:w-[480px] xl:h-[400px] xl:w-[610px]'>
-        <Image
-          src={`/img/gifting/guide${index + 1}.png`}
-          layout='fill'
-          className='object-cover'
-          alt='Guide image'
-        />
-        <div className='bg-opacity-50 absolute right-0 bottom-0 left-0 p-1 text-center text-xs font-medium text-white sm:text-sm lg:text-xl'>
-          Eternal Rings (400 items)
-        </div>
-      </div>
-    </SwiperSlide>
-  ))}
-</div>
-</Swiper>
-*/
