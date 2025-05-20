eaf517d6d5b2686123544b11801e4356bb4a345c
Anjaliorpol <anjaliorpol@gmail.com>
2025-05-20 15:21:02 +0530

    wishlist length skeleton

--- src/store/wishlist-store.js
+++ src/store/wishlist-store.js
@@ -3,11 +3,9 @@ import { useUserStore } from './user-store'; // adjust the path if needed
 
 export const useWishlistStore = create((set, get) => ({
   wishlist: [],
-  isLoading: false,
   error: null,
 
   fetchWishlist: async () => {
-    set({ isLoading: true });
     const { authUser } = useUserStore.getState();
     if (authUser?.id) {
       try {
@@ -21,8 +19,6 @@ export const useWishlistStore = create((set, get) => ({
       } catch (err) {
         console.error('Error loading wishlist:', err);
         set({ error: err });
-      } finally {
-        set({ isLoading: false });
       }
     }
   },
